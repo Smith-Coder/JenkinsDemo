754ddf417209629d01c561200eee8758f444f8ae
Smith C D <smithcd438@gmail.com>
2023-03-28 14:45:34 +0530

    updating TestClass

--- TestClass.java
+++ TestClass.java
@@ -1,1 +1,10 @@
-console.log("hello world");
+package JenkinsDemo;
+
+/**
+ * TestClass
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
